d08522a18e31606dd0a64d0441e04c31dc541f09
gdetam <hellostyle@mail.ru>
2021-01-15 18:06:09 +0300

    logout user by 'logout' button in menu

--- routers/loader_routers.py
+++ routers/loader_routers.py
@@ -6,6 +6,7 @@ from . import create_recipe_router
 from . import delete_recipe_router
 from . import home_page_router
 from . import login_router
+from . import logout_router
 from . import random_recipe_router
 from . import recipe_detail_router
 from . import recipes_router
---
+++ routers/logout_router.py
@@ -0,0 +1,14 @@
+"""this file router for logout page."""
+
+from app import app
+
+from flask import redirect, url_for
+
+from flask_login import logout_user
+
+
+@app.route('/logout')
+def logout():
+    """Router for logout page."""
+    logout_user()
+    return redirect(url_for('home'))
